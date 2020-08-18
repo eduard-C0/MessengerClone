71f4577ee4257707e29149c532697deb6c0ca594
eduard-C0 <57216274+eduard-C0@users.noreply.github.com>
2020-08-18 16:51:26 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import { Button } from '@material-ui/core';
 import { FormControl, InputLabel, Input} from '@material-ui/core';
 import Message from './Message.js';
-import db from './firebase.js'
-import firebase from 'firebase'
+import db from './firebase.js';
+import firebase from 'firebase';
+import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
+import {IconButton} from '@material-ui/core';
 
 function App() {
+
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [username, setUsername] = useState('');
+
   // useState is how you declare a variable in React
   // useEffect = run code on a condition in React
 
@@ -17,7 +22,7 @@ function App() {
     db.collection('messages')
     .orderBy('timestamp','desc')
     .onSnapshot(snapshot => {
-      setMessages(snapshot.docs.map(doc => doc.data()))
+      setMessages(snapshot.docs.map(doc =>({id: doc.id, message: doc.data()})))
     })
 
   }, [])
@@ -38,23 +43,21 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome {username}</h1>
-        <form>
-
-        <FormControl>
-            <InputLabel >Enter a message</InputLabel>
-            <Input value={input} onChange={event => setInput(event.target.value)}/>
-            <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Messange
-            </Button>
-        </FormControl>
-         
+        <form className="app__form">
+          <FormControl className="app__formControl">
+              <Input className="app__input" placeholder='Enter a message... ' value={input} onChange={event => setInput(event.target.value)}/>
+              <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>
+                <SendIcon color="primary"/>
+              </IconButton>
+          </FormControl>
         </form>
-
-
-        {
-          messages.map(message => (
-            <Message username={username} message={message} />
-          ))
-        }
+        <FlipMove>
+          {
+            messages.map(({id, message}) => (
+              <Message key={id} username={username} message={message} />
+            ))
+          }
+        </FlipMove>
     </div>
   );
 }
